10c41fdb2a6c777e51d9d7aa33d3167676cb3aaa
cofdream <cofdream@sina.com>
2020-11-02 11:01:35 +0800

    todo 包的版本控制工具

---
+++ Assets/Scripts/Editor/PackageTool/PackageVersionTool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DATools
+{
+    public class PackageVersionTool : EditorWindow
+    {
+        private void OnGUI()
+        {
+            // todo 获取git的版本
+
+            // 用户选择对应版本去更新
+        }
+
+
+        [MenuItem("Tools/VersionUpdate")]
+        private static void OpenPackageVersionTool()
+        {
+            var window = EditorMainWindow.GetWindowInCenter<PackageVersionTool>();
+            window.Show();
+        }
+    }
+}
