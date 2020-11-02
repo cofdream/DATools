8fda6ba7c40c45de5e2f968a9c4a5c544348ccb2
cofdream <cofdream@sina.com>
2020-11-02 16:48:06 +0800

    update

--- Assets/Scripts/Editor/InspectorExpand/TransformInspector.cs
+++ Assets/Scripts/Editor/InspectorExpand/TransformInspector.cs
@@ -47,8 +47,6 @@ namespace DATools
             serializedPropertyPosition = m_serializedObject.FindProperty("addPosition");
             serializedPropertyRotation = m_serializedObject.FindProperty("addRotation");
             serializedPropertyScale = m_serializedObject.FindProperty("addScale");
-
-            Debug.Log("Test update");
         }
         private void OnDisable()
         {
--- Assets/Scripts/Editor/PackageTool/PackageVersionTool.cs
+++ Assets/Scripts/Editor/PackageTool/PackageVersionTool.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEditor.UIElements;
 
 namespace DATools
 {
@@ -7,10 +8,41 @@ namespace DATools
     {
         private void OnGUI()
         {
-            // todo 获取git的版本
+            // todo 获取git的版本l
 
             // 用户选择对应版本去更新
+
+            //if (GUILayout.Button("Test"))
+            //{
+
+            //    var newThread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(CmdOpenDirectory));
+            //    newThread.Start();
+
+            //}
+
         }
+
+        //private static void CmdOpenDirectory(object obj)
+        //{
+        //    var p = new System.Diagnostics.Process();
+        //    p.StartInfo.FileName = "cmd.exe";
+        //    p.StartInfo.Arguments = @"E:\Git\DATools git tags";
+
+        //    UnityEngine.Debug.Log(p.StartInfo.Arguments);
+
+        //    p.StartInfo.UseShellExecute = false;
+        //    p.StartInfo.RedirectStandardInput = true;
+        //    p.StartInfo.RedirectStandardOutput = true;
+        //    p.StartInfo.RedirectStandardError = true;
+        //    p.StartInfo.CreateNoWindow = true;
+        //    p.Start();
+
+        //    p.WaitForExit();
+
+        //    Debug.Log(p.StandardOutput.ReadToEnd());
+
+        //    p.Close();
+        //}
 
 
         [MenuItem("Tools/VersionUpdate")]
