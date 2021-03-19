cb7bbf3b7367e0b027e1932ac573ed83d2964328
cofdream <cofdream@sina.com>
2021-03-19 08:32:53 +0800

    添加寻找工具窗口

---
+++ Scripts/Editor/FindDAToolWindow/SearchToolsAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DATools
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    internal class SearchToolsAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public string[] Keywords { get; private set; }
+
+        public SearchToolsAttribute(string keyword)
+        {
+            Name = keyword;
+            Keywords = new string[] { keyword.ToLower() };
+        }
+        public SearchToolsAttribute(string name, string keyword)
+        {
+            Name = name;
+            Keywords = new string[] { keyword.ToLower() };
+        }
+        public SearchToolsAttribute(string name, string[] keywords)
+        {
+            Name = name;
+            if (keywords == null)
+                Keywords = new string[0];
+            else
+            {
+                int index = 0;
+                foreach (var keyword in keywords)
+                {
+                    Keywords[index] = keyword.ToLower();
+                    index++;
+                }
+            }
+        }
+    }
+}
