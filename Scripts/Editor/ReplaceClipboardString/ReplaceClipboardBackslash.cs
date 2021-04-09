95f5246ca1c09b5fd44f135ef4981ea2ea6185fb
cofdream <cofdream@sina.com>
2021-04-09 11:17:18 +0800

    add 替换粘贴板内容工具

---
+++ Scripts/Editor/ReplaceClipboardString/ReplaceClipboardBackslash.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DATools
+{
+    public class ReplaceClipboardString : EditorWindow, IDevelopementTool
+    {
+        public string ToolName => "替换粘体板字符串";
+
+        [SearchTools("替换粘体板字符串")]
+        [MenuItem("DATools/Replace Clipboard String Window")]
+        static void OpenWindow()
+        {
+            EditorMainWindow.GetWindowInCenter<ReplaceClipboardString>().Show();
+        }
+
+
+        public void OnGUI()
+        {
+            if (GUILayout.Button("替换粘贴板反斜杠为斜杠"))
+            {
+                GUIUtility.systemCopyBuffer = ReplaceBackslash(GUIUtility.systemCopyBuffer);
+            }
+        }
+        public string ReplaceBackslash(string content)
+        {
+            return content.Replace('\\', '/');
+        }
+
+        public void Awake()
+        {
+        }
+
+        public void OnDestroy()
+        {
+        }
+
+        public void OnEnable()
+        {
+        }
+
+        public void OnDisable()
+        {
+        }
+
+    }
+}
