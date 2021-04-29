e0bfb73ca52e4cf43f8b61e640ecd30abe0f0e25
cofdream <cofdream@sina.com>
2021-04-29 10:20:36 +0800

    修改显示射线方式，抛弃继承mono形式

--- Scripts/Editor/DevelopmentTool/UI/UIDrawRaycast.cs
+++ Scripts/Editor/DevelopmentTool/UI/UIDrawRaycast.cs
@@ -8,44 +8,19 @@ namespace DATools
     {
         public string ToolName => "UIDrawRaycast";
 
-        private DrawGizmosUIRaycast drawGameObject;
-        private bool isShow;
-        private bool IsShow
-        {
-            get { return isShow; }
-            set
-            {
-                if (isShow == value) return;
-
-                isShow = value;
-                drawGameObject?.gameObject.SetActive(value);
-            }
-        }
-
         private int selectIndex;
-        private string[] colorArray;
+        private static string[] colorArray;
 
         public void OnDestroy()
         {
             colorArray = null;
-
-            if (drawGameObject != null)
-            {
-                DestroyImmediate(drawGameObject.gameObject);
-                drawGameObject = null;
-            }
-            else
-            {
-                ClearAllInScreenDrawGizmosObject();
-            }
         }
 
         public void Awake()
         {
             isShow = false;
-            CreateDrawGameObject();
 
-            selectIndex = 5;
+            selectIndex = 0;
             colorArray = new string[]
             {
                 "Red", "Green", "Blue","white","black", "yellow", "cyan","magenta","gray", "grey", "clear"
@@ -54,50 +29,25 @@ namespace DATools
 
         public void OnGUI()
         {
-            if (drawGameObject == null)
-            {
-                CreateDrawGameObject();
-            }
-
-            IsShow = GUILayout.Toggle(IsShow, "绘制UI射线");
+            isShow = GUILayout.Toggle(isShow, "绘制UI射线");
             selectIndex = EditorGUILayout.Popup(selectIndex, colorArray);
 
             switch (selectIndex)
             {
-                case 0: drawGameObject.DrawColor = Color.red; break;
-                case 1: drawGameObject.DrawColor = Color.green; break;
-                case 2: drawGameObject.DrawColor = Color.blue; break;
-                case 3: drawGameObject.DrawColor = Color.white; break;
-                case 4: drawGameObject.DrawColor = Color.black; break;
-                case 5: drawGameObject.DrawColor = Color.yellow; break;
-                case 6: drawGameObject.DrawColor = Color.cyan; break;
-                case 7: drawGameObject.DrawColor = Color.magenta; break;
-                case 8: drawGameObject.DrawColor = Color.gray; break;
-                case 9: drawGameObject.DrawColor = Color.grey; break;
-                case 10: drawGameObject.DrawColor = Color.clear; break;
+                case 0: drawColor = Color.red; break;
+                case 1: drawColor = Color.green; break;
+                case 2: drawColor = Color.blue; break;
+                case 3: drawColor = Color.white; break;
+                case 4: drawColor = Color.black; break;
+                case 5: drawColor = Color.yellow; break;
+                case 6: drawColor = Color.cyan; break;
+                case 7: drawColor = Color.magenta; break;
+                case 8: drawColor = Color.gray; break;
+                case 9: drawColor = Color.grey; break;
+                case 10: drawColor = Color.clear; break;
             }
         }
 
-        private void CreateDrawGameObject()
-        {
-            ClearAllInScreenDrawGizmosObject();
-
-            drawGameObject = new GameObject("[Draw Gizmos UI Raycast]").AddComponent<DrawGizmosUIRaycast>();
-            drawGameObject.gameObject.SetActive(isShow);
-
-            if (Application.isPlaying)
-            {
-                DontDestroyOnLoad(drawGameObject.gameObject);
-            }
-        }
-        private void ClearAllInScreenDrawGizmosObject()
-        {
-            var drawObjs = GameObject.FindObjectsOfType<DrawGizmosUIRaycast>();
-            foreach (var drawObj in drawObjs)
-            {
-                DestroyImmediate(drawObj);
-            }
-        }
 
         public void OnEnable()
         {
@@ -109,30 +59,25 @@ namespace DATools
 
         }
 
-        private sealed class DrawGizmosUIRaycast : MonoBehaviour
-        {
-            public Color DrawColor;
-            static Vector3[] fourCorners = new Vector3[4];
-            void OnDrawGizmos()
-            {
-                foreach (Graphic graphic in GameObject.FindObjectsOfType<Graphic>())
-                {
-                    if (graphic.raycastTarget)
-                    {
-                        RectTransform rectTransform = graphic.transform as RectTransform;
-                        DrawRect(rectTransform);
-                    }
-                }
-            }
+        private static bool isShow;
+        private static Color drawColor;
+        private static Vector3[] fourCorners = new Vector3[4];
 
-            void DrawRect(RectTransform rectTransform)
+
+        [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected, typeof(Graphic))]
+        private static void DrawRayRect(Graphic graphic, GizmoType gizmoType)
+        {
+            if (isShow)
             {
-                if (rectTransform == null) return;
-                rectTransform.GetWorldCorners(fourCorners);
-                Gizmos.color = DrawColor;
-                for (int i = 0; i < 4; i++)
+                var rect = graphic.GetComponent<RectTransform>();
+                if (rect != null)
                 {
-                    Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+                    rect.GetWorldCorners(fourCorners);
+                    Gizmos.color = drawColor;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+                    }
                 }
             }
         }
