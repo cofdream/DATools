d4b60d0ca61bc457dd6a9f5492cb33a5a51af59a
cofdream <cofdream@sina.com>
2021-04-29 17:40:23 +0800

    update

---
+++ Assets/DATools/Scripts/Editor/CMD/CMDData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DATools
+{
+    public class CMDData : ScriptableObject
+    {
+        public CMDElement[] cmds;
+    }
+
+    [System.Serializable]
+    public class CMDElement
+    {
+        public string caption;
+        public bool State;
+        public CMD CMD;
+       
+    }
+}
