af50d4c4dc0450825f24682f251f6172fb1d6ab1
cofdream <cofdream@sina.com>
2021-06-03 18:00:12 +0800

    编辑器滑动分割工具制作中...

---
+++ Scripts/Editor/GUISplitter/GUISplitView.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DATools
+{
+
+    public struct GUISplitData
+    {
+        public bool IsHorizontal;
+        public Vector2 ViewPosition;
+        public float Width;
+        public float Height;
+    }
+    public static class GUISplitView
+    {
+        public static void BeginSplitView(ref GUISplitData data)
+        {
+            if (data.IsHorizontal)
+                data.ViewPosition = GUILayout.BeginScrollView(data.ViewPosition, GUILayout.Width(data.Width), GUILayout.Width(data.Height));
+            else
+                data.ViewPosition = GUILayout.BeginScrollView(data.ViewPosition, GUILayout.Width(data.Width), GUILayout.Height(data.Height));
+        }
+
+        public static void EndSplitView(ref GUISplitData data)
+        {
+            GUILayout.EndScrollView();
+            ResizeSplitView(ref data);
+        }
+
+        private static void ResizeSplitView(ref GUISplitData data)
+        {
+            Rect linePosition;
+            if (data.IsHorizontal)
+            {
+                
+            }
+        }
+    }
+}
