5a297ceaef972b6150ea79a82e9fd74fcf272983
cofdream <cofdream@sina.com>
2021-06-04 13:41:09 +0800

    代码完善

--- Scripts/Editor/GUISplitter/GUISplitView.cs
+++ Scripts/Editor/GUISplitter/GUISplitView.cs
@@ -3,33 +3,123 @@ using UnityEditor;
 
 namespace DATools
 {
-    public enum LinePos
-    {
-        Left = 0,
-        Right = 1,
-        Top = 2,
-        Bottom = 3,
-    }
     public struct GUISplitData
     {
-        public LinePos LinePos;
         public Vector2 ViewPosition;
+
         public float X;
         public float Y;
         public float Width;
         public float Height;
 
+        public float MinWidth;
+        public float MinHeight;
+
         public float MaxWidth;
         public float MaxHeight;
 
-        public bool Resize;
+        public bool IsSplitHorizontal;
+        public bool IsSplitVertiacal;
+
+        public bool ResizeHorizontal;
+        public bool ResizeVertical;
+
+        //public float LineSpace;
+        //public float LineSpaceOffest;
+        //public float lineWidth;
+
+        public GUISplitData(float x, float y, float width, float height, float minWidth, float minHeight, float maxWidth, float maxHeight, bool isSplitHorizontal, bool isSplitVertiacal)
+        {
+            ViewPosition = Vector2.zero;
+
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+
+            MinWidth = minWidth;
+            MinHeight = minHeight;
+            MaxWidth = maxWidth;
+            MaxHeight = maxHeight;
+
+            IsSplitHorizontal = isSplitHorizontal;
+            IsSplitVertiacal = isSplitVertiacal;
+
+            ResizeHorizontal = false;
+            ResizeVertical = false;
+
+            //LineSpace = 5;
+            //LineSpaceOffest = 2;
+            //lineWidth = 1;
+        }
     }
     public static class GUISplitView
     {
-        public static void BeginSplitView(ref GUISplitData data)
+
+        #region 整合ScroolView API
+
+        public static void BeginSplitView(ref GUISplitData data, params GUILayoutOption[] options)
         {
-            data.Resize = false;
-            data.ViewPosition = GUILayout.BeginScrollView(data.ViewPosition, GUILayout.Width(data.Width), GUILayout.Height(data.Height));
+            BeginSplitView(ref data, false, false, GUI.skin.horizontalScrollbar, GUI.skin.verticalScrollbar, GUI.skin.scrollView, options);
+        }
+
+        public static void BeginSplitView(ref GUISplitData data, bool alwaysShowHorizontal, bool alwaysShowVertical, params GUILayoutOption[] options)
+        {
+            BeginSplitView(ref data, alwaysShowHorizontal, alwaysShowVertical, GUI.skin.horizontalScrollbar, GUI.skin.verticalScrollbar, GUI.skin.scrollView, options);
+        }
+
+        public static void BeginSplitView(ref GUISplitData data, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar, params GUILayoutOption[] options)
+        {
+            BeginSplitView(ref data, alwaysShowHorizontal: false, alwaysShowVertical: false, horizontalScrollbar, verticalScrollbar, GUI.skin.scrollView, options);
+        }
+
+
+        public static void BeginSplitView(ref GUISplitData data, GUIStyle style)
+        {
+            BeginSplitView(ref data, style, options: null);
+        }
+
+        public static void BeginSplitView(ref GUISplitData data, GUIStyle style, params GUILayoutOption[] options)
+        {
+            string name = style.name;
+            GUIStyle gUIStyle = GUI.skin.FindStyle(name + "VerticalScrollbar");
+            if (gUIStyle == null)
+            {
+                gUIStyle = GUI.skin.verticalScrollbar;
+            }
+
+            GUIStyle gUIStyle2 = GUI.skin.FindStyle(name + "HorizontalScrollbar");
+            if (gUIStyle2 == null)
+            {
+                gUIStyle2 = GUI.skin.horizontalScrollbar;
+            }
+
+            BeginSplitView(ref data, alwaysShowHorizontal: false, alwaysShowVertical: false, gUIStyle2, gUIStyle, style, options);
+        }
+
+        public static void BeginSplitView(ref GUISplitData data, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar, params GUILayoutOption[] options)
+        {
+            BeginSplitView(ref data, alwaysShowHorizontal, alwaysShowVertical, horizontalScrollbar, verticalScrollbar, GUI.skin.scrollView, options);
+        }
+
+        #endregion
+
+        public static void BeginSplitView(ref GUISplitData data, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar, GUIStyle background, params GUILayoutOption[] options)
+        {
+            GUILayoutOption[] newOptions;
+            if (options != null)
+            {
+                newOptions = new GUILayoutOption[options.Length + 2];
+                options.CopyTo(newOptions, 2);
+            }
+            else
+                newOptions = new GUILayoutOption[2];
+
+
+            newOptions[0] = GUILayout.Width(data.Width);
+            newOptions[1] = GUILayout.Height(data.Height);
+
+            data.ViewPosition = GUILayout.BeginScrollView(data.ViewPosition, alwaysShowHorizontal, alwaysShowVertical, horizontalScrollbar, verticalScrollbar, background, newOptions);
         }
 
         public static void EndSplitView(ref GUISplitData data)
@@ -40,108 +130,101 @@ namespace DATools
 
         private static void ResizeSplitView(ref GUISplitData data)
         {
-            Rect splitRect;
-
-            float lineSpace = 3f;
-            float lineSpaceOffest = 1f; //减少线段的宽度
+            float lineSpace = 5f;
+            float lineSpaceOffest = 2f; //=lineSpace * 0.5 - 线段的宽度
             Color lineColor = new Color(0, 0, 0, 0.5f);
 
-            switch (data.LinePos)
+
+            //Rect resizeUpLeft = new Rect(data.X + data.Width - lineSpaceOffest, data.Y + data.Height - lineSpaceOffest, lineSpace, lineSpace);
+            //EditorGUIUtility.AddCursorRect(resizeUpLeft, MouseCursor.ResizeUpLeft);
+
+            //if (Event.current.type == EventType.MouseDown && resizeUpLeft.Contains(Event.current.mousePosition))
+            //{
+            //    data.ResizeVertical = true;
+            //    data.ResizeHorizontal = true;
+            //}
+
+            //if (Event.current.type == EventType.MouseUp)
+            //{
+            //    data.ResizeVertical = false;
+            //    data.ResizeHorizontal = false;
+            //}
+
+
+            if (data.IsSplitHorizontal)
             {
-                case LinePos.Left:
-                    {
-                        splitRect = new Rect(data.X - lineSpaceOffest, data.Y, lineSpace, data.Height);
-                        EditorGUIUtility.AddCursorRect(splitRect, MouseCursor.ResizeHorizontal);
+                Rect splitResizeLeftRightRect = new Rect(data.X + data.Width - lineSpaceOffest, data.Y, lineSpace, data.Height);
+                EditorGUIUtility.AddCursorRect(splitResizeLeftRightRect, MouseCursor.SplitResizeLeftRight);
 
-                        EditorGUI.DrawRect(splitRect, Color.white);
+                //EditorGUI.DrawRect(splitResizeLeftRightRect, new Color(1, 1, 1, 0.1f));
 
-
-                        Rect lineRect = splitRect;
-                        lineRect.x += lineSpaceOffest;
-                        lineRect.width = 1f;
-                        EditorGUI.DrawRect(lineRect, lineColor);
-                        break;
-                    }
-                case LinePos.Right:
-                    {
-                        splitRect = new Rect(data.X + data.Width - lineSpaceOffest, data.Y, lineSpace, data.Height);
-                        EditorGUIUtility.AddCursorRect(splitRect, MouseCursor.ResizeHorizontal);
-
-                        EditorGUI.DrawRect(splitRect, Color.white);
-
-                        Rect lineRect = splitRect;
-                        lineRect.x += lineSpaceOffest;
-                        lineRect.width = 1f;
-                        EditorGUI.DrawRect(lineRect, lineColor);
-                        break;
-                    }
-                case LinePos.Top:
-                    {
-                        splitRect = new Rect(data.X, data.Y - lineSpaceOffest, data.Width, lineSpace);
-                        EditorGUIUtility.AddCursorRect(splitRect, MouseCursor.ResizeVertical);
-
-                        EditorGUI.DrawRect(splitRect, Color.white);
-
-                        Rect lineRect = splitRect;
-                        lineRect.y += lineSpaceOffest;
-                        lineRect.height = 1f;
-                        EditorGUI.DrawRect(lineRect, lineColor);
-                        break;
-                    }
-                case LinePos.Bottom:
-                    {
-                        splitRect = new Rect(data.X, data.Y + data.Height - lineSpaceOffest, data.Width, lineSpace);
-                        EditorGUIUtility.AddCursorRect(splitRect, MouseCursor.ResizeVertical);
-
-                        EditorGUI.DrawRect(splitRect, Color.white);
-
-                        Rect lineRect = splitRect;
-                        lineRect.y += lineSpaceOffest;
-                        lineRect.height = 1f;
-                        EditorGUI.DrawRect(lineRect, lineColor);
-                        break;
-                    }
-                default:
-                    return;
-            }
-
-            if (Event.current.type == EventType.MouseDown && splitRect.Contains(Event.current.mousePosition))
-                isMove = true;
-
-            if (Event.current.type == EventType.MouseUp)
-                isMove = false;
+                Rect lineRect = splitResizeLeftRightRect;
+                lineRect.x += lineSpaceOffest;
+                lineRect.width = 1f;
+                EditorGUI.DrawRect(lineRect, lineColor);
 
 
-            if (isMove && Event.current.type == EventType.MouseDrag)
-            {
-                if ((int)data.LinePos < 2)
+
+                if (Event.current.type == EventType.MouseDown && splitResizeLeftRightRect.Contains(Event.current.mousePosition))
+                    data.ResizeHorizontal = true;
+
+                if (Event.current.type == EventType.MouseUp)
+                    data.ResizeHorizontal = false;
+
+                if (data.ResizeHorizontal && Event.current.type == EventType.MouseDrag)
                 {
-                    data.Width =  Event.current.mousePosition.x;
+                    data.Width = Event.current.mousePosition.x;
                     if (data.Width > data.MaxWidth)
                     {
                         data.Width = data.MaxWidth;
                     }
-                    else if (data.Width < 0)
+                    else if (data.Width < data.MinWidth)
                     {
-                        data.Width = 0;
+                        data.Width = data.MinWidth;
                     }
                 }
-                else
+            }
+
+            if (data.IsSplitVertiacal)
+            {
+                Rect splitResizeUpDownRect = new Rect(data.X, data.Y + data.Height - lineSpaceOffest, data.Width, lineSpace);
+                EditorGUIUtility.AddCursorRect(splitResizeUpDownRect, MouseCursor.ResizeVertical);
+
+                //EditorGUI.DrawRect(splitResizeUpDownRect, new Color(1, 1, 1, 0.1f));
+
+                Rect lineRect = splitResizeUpDownRect;
+                lineRect.y += lineSpaceOffest;
+                lineRect.height = 1f;
+                EditorGUI.DrawRect(lineRect, lineColor);
+
+
+
+                if (Event.current.type == EventType.MouseDown && splitResizeUpDownRect.Contains(Event.current.mousePosition))
+                    data.ResizeVertical = true;
+
+                if (Event.current.type == EventType.MouseUp)
+                    data.ResizeVertical = false;
+
+
+                if (data.ResizeVertical && Event.current.type == EventType.MouseDrag)
                 {
+                    EditorGUIUtility.AddCursorRect(splitResizeUpDownRect, MouseCursor.SplitResizeUpDown);
+
                     data.Height = Event.current.mousePosition.y;
                     if (data.Height > data.MaxHeight)
                     {
                         data.Height = data.MaxHeight;
                     }
-                    else if (data.Height < 0)
+                    else if (data.Height < data.MinHeight)
                     {
-                        data.Height = 0;
+                        data.Height = data.MinHeight;
                     }
                 }
-                data.Resize = true;
             }
+
+           
         }
 
-        public static bool isMove;
+        //todo Check GUISplitData 的范围，保证在窗口内
     }
 }
