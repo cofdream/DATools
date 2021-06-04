1e9bc2139a5a1fac1df65e212e9a82c077b21bac
cofdream <cofdream@sina.com>
2021-06-04 17:25:48 +0800

    add 创建asset资源工具

---
+++ Scripts/Editor/Other/CreateAsset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace DATools
+{
+    [CreateAssetMenu(menuName = "Asset File", fileName = "NewAsset")]
+    internal sealed class CreateAsset : ScriptableObject
+    {
+    }
+
+    [CustomEditor(typeof(CreateAsset), false)]
+    internal class CreateAssetInspector : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            SerializedObject serializedObject = new SerializedObject(target);
+            serializedObject.Update();
+
+            SerializedProperty m_Script = serializedObject.FindProperty("m_Script");
+            EditorGUILayout.PropertyField(m_Script);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
